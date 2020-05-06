9654973ec76ef8c594d0b4b5665b90dcc0ed8703
Nam2210 <53987452+Nam2210@users.noreply.github.com>
2020-05-06 22:52:19 +0700

    update

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+session_start();
+// tôi muốn: "tất cả các request tới thư mục mvc phải bắt buộc đi qua file index.php"
+// thu thập các url gửi lên project
+$url = isset($_GET['url']) ? $_GET['url'] : "/";
+
+require_once './vendor/autoload.php';
+require_once './commons/database-config.php';
+
+use App\Controllers\HomeController;
+use App\Controllers\ProductController;
+use App\Controllers\CategoryController;
+
+// routing
+switch ($url) {
+    case '/':
+        $ctr = new HomeController();
+        $ctr->index();
+        break;
+    
+    case 'detail':
+        $ctr = new HomeController();
+        $ctr->detail();
+        break;
+    case 'insert-cate':
+        $ctr = new CategoryController();
+        $ctr->addData();
+        break;
+    
+    case 'update-cate':
+        $ctr = new CategoryController();
+        $ctr->editData();
+        break;
+
+    case 'delete-cate':
+        $ctr = new CategoryController();
+        $ctr->delete();
+        break;
+    case 'list-cate':
+        $ctr = new CategoryController();
+        $ctr->index();
+        break;
+    
+    case 'contact':
+        $ctr = new HomeController();
+        $ctr->contact();
+        break;
+
+    case 'add-product':
+        $ctr = new ProductController();
+        $ctr->addForm();
+        break;
+    case 'save-add-product':
+        $ctr = new ProductController();
+        $ctr->saveAdd();
+        break;
+    case 'remove-product':
+        $ctr = new ProductController();
+        $ctr->remove();
+        break;
+    case 'edit-product':
+        $ctr = new ProductController();
+        $ctr->editForm();
+        break;
+    case 'save-edit-product':
+        $ctr = new ProductController();
+        $ctr->saveEdit();
+        break;
+    case 'giao-dien':
+        $ctr = new HomeController();
+        $ctr->giaoDien();
+        break;
+    default:
+        echo "Đường dẫn không tồn tại";
+        break;
+}
+
+?>
